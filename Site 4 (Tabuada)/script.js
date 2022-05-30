62eb1678c9a540dc7e17157c77373054be373ec1
caiohbe <caiohbe@gmail.com>
2022-05-30 20:01:03 -0300

    Comentários no Site 4

--- Site 4 (Tabuada)/script.js
+++ Site 4 (Tabuada)/script.js
@@ -1,16 +1,16 @@
-function gerar() {
-    let num = (document.querySelector('#numtxt').value)
-    let tab = document.querySelector('#seltab')
+function gerar() { // Cria a função que funcionará ao receber "gerar()"
+    let num = (document.querySelector('#numtxt').value) // Atribui à variável num, o valor escrito na caixa HTML
+    let tab = document.querySelector('#seltab') // Atribui à variavel tab, a tabela no HTML
 
-    if (num.length == 0) {
-        alert('Digite um número')
-    } else {
-        let n = Number(num)
-        tab.innerHTML = ''
-        for (i = 1; i <= 10; i++) {
-            let item = document.createElement('option')
-            item.text = `${n} x ${i} = ${n*i}`
-            tab.appendChild(item)
+    if (num.length == 0) { // Cria uma condição 
+        alert('Digite um número') // Alertará caso a condição for atendida
+    } else { // Se a condição não for atendida
+        let n = Number(num) // Atribuí o número que estava na caixa HTML a uma variável
+        tab.innerHTML = '' // Deixa a tabela vazia para não existir sobreposição de dados
+        for (i = 1; i <= 10; i++) { // Cria o loop de 1 até 10
+            let item = document.createElement('option') // Pra cada loop cria um item 'option' para a tabela
+            item.text = `${n} x ${i} = ${n*i}` // Da ao texto do item conteúdo
+            tab.appendChild(item) // Adiciona o item para a tabela "tab", uma vez que 'option' é child de 'select'
         }
     }
 }
